b10d4e1870a309fda6e43dde135485b43849bcd8
1024小神 <48399687+Sjj1024@users.noreply.github.com>
2023-10-12 11:11:14 +0800

    Update config.py

--- config.py
+++ config.py
@@ -7,16 +7,22 @@ LOG_LEVEL = logging.DEBUG
 LOG_FORMAT = "%(asctime)s - %(levelname)s - %(message)s"
 
 # 直播信息配置：直播地址，直播用户排名，直播排名抓取间隔，直播推送到后台，推送到后台地址
-LIVE_ROOM_URL = ""
+LIVE_ROOM_URL = "https://live.douyin.com/355239620902"
+# 特殊礼物单独统计
+LIVE_GIFT_LIST = ["小心心"]
 # 是否抓取在线打赏排名
 LIVE_RANK_LIST = True
 # 获取礼物排名时间间隔: 建议不要低于10秒
-LIVE_RANK_INTERVAL = 15
+LIVE_RANK_INTERVAL = 10
 # 使用ws推送直播数据
 LIVE_WEB_SEND = False
 # 是否开启HTTP推送
 LIVE_HTTP_SEND = True
+# 多久向服务端推送一条消息
+LIVE_SEND_INTERVAL = 3
 # HTTP推送地址
-LIVE_WEB_SEND_URL = "http://dev.*******/game/gamemgnt"
-# 应援UUID
-DONATION_UUID = "efa3fc3d-efb8-4201-82fa-92d9aeaeb98d"
+LIVE_WEB_SEND_URL = "*******"
+# 一场比赛唯一的UUID
+GAME_UUID = "157ae45b-263b-414a-8976-6d2ad210a7e8"
+# 应援UUID(这是我们自己项目推送使用的参数)
+DONATION_UUID = "a749cffa-510f-4b6b-a711-691040d69079"
