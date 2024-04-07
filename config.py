170ec02a660a5dfdc3d2e4f2c98942f0cda0188b
1024小神 <15670339118@163.com>
2024-04-07 10:43:23 +0800

    add mac

--- config.py
+++ config.py
@@ -7,7 +7,7 @@ LOG_LEVEL = logging.DEBUG
 LOG_FORMAT = "%(asctime)s - %(levelname)s - %(message)s"
 
 # 直播信息配置：直播地址，直播用户排名，直播排名抓取间隔，直播推送到后台，推送到后台地址
-LIVE_ROOM_URL = "https://live.douyin.com/352577626339"
+LIVE_ROOM_URL = "https://live.douyin.com/269048052485"
 # 特殊礼物单独统计
 LIVE_GIFT_LIST = ["月下瀑布"]
 # 是否抓取在线打赏排名
